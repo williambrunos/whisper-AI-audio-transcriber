824d8e6f875e606572fe4c0138327fb5fc252a35
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2024-07-23 10:59:49 -0300

    feat: adding python script that transcribes audio files into text

---
+++ main.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+audio_file = open("your_audio_file", "rb")
+
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file, 
+  response_format="text",
+  language="pt"
+)
+
+# Salvar a transcrição em um arquivo .txt
+with open("transcricao.txt", "w", encoding="utf-8") as file:
+    file.write(transcription)
+
+print("Transcrição salva em transcricao.txt")
